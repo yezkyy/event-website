a6ebd62c220f6f5f27c933f83cea2a592050ac9d
yezkyy <muhammadrizkys2004@gmail.com>
2025-01-06 12:41:00 +0800

    Refactor styles and structure for improved layout and design

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Schedule from "./components/Schedule";
 import Speakers from "./components/Speakers";
 import RegistrationForm from "./components/RegistrationForm";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
